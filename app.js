d97afb70ba6a67f15cc36cde42bc101208112ee1
Franco <francosalvadeo1@gmail.com>
2023-07-15 19:30:34 -0300

    testing the responsive part of the project

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-window.onscroll = function() {slideOnScrollLeft("logo2djs", 1), slideOnScrollLeft("logo2djs2", 1750)};
+let scrollQ_secondlogo = 0;
+
+if(screen.width <= 450) {
+  scrollQ_secondlogo = 200} else if (screen.width <= 600) {
+    scrollQ_secondlogo = 350
+  } else {
+    scrollQ_secondlogo = 1750  
+  };
+
+window.onscroll = function() {slideOnScrollLeft("logo2djs", 1), slideOnScrollLeft("logo2djs2", scrollQ_secondlogo)};
 
 function slideOnScrollLeft(selectedElement, scrollPosition) {
   if (document.body.scrollTop > scrollPosition || document.documentElement.scrollTop > scrollPosition) {
